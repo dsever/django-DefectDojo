12ed819dcbd88c7b8dcca84e7480b6dc6244324e
Dubravko Sever <dubravko.sever@pan-net.eu>
2020-08-20 11:33:27 +0200

    Add: messaage

--- travis/script.sh
+++ travis/script.sh
@@ -154,6 +154,7 @@ if [ -z "${TEST}" ]; then
   travis_fold end minikube_install
 
   travis_fold start check_container_user
+  echo "Checkig dd user is not root" 
   sudo kubectl exec -i $(kubectl get pods -o name  | grep django) -c uwsgi id
   travis_fold end check_container_user
   # Run all tests
