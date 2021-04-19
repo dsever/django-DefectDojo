d0b3e7b0c050d49001e7a1b8c1412040a773cf38
Dubravko Sever <dubravko.sever@pan-net.eu>
2021-04-19 23:14:06 +0200

    Add: ToolType form order Add: Integration tests

---
+++ tests/disable_tool_type_test.py
@@ -0,0 +1,71 @@
+import unittest
+import sys
+import os
+
+from base_test_class import BaseTestCase
+
+
+class ScanTypeTest(BaseTestCase):
+
+    def login_page(self):
+        driver = self.driver
+        driver.get(self.base_url + "login")
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys(os.environ['DD_ADMIN_USER'])
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys(os.environ['DD_ADMIN_PASSWORD'])
+        driver.find_element_by_css_selector("button.btn.btn-success").click()
+        return driver
+
+    def test_disable_Acunetix_Scan_scanner(self):
+        driver = self.driver
+        driver.get(self.base_url + "tool_type")
+        driver.find_element_by_link_text("Acunetix Scan").click()
+        checkbox = driver.find_element_by_id("id_enabled")
+        if checkbox.is_selected():
+            checkbox.click()
+
+        driver.find_element_by_css_selector(".col-sm-offset-2 > .btn").click()
+        self.assertTrue(self.is_success_message_present(text="Tool Type Successfully Updated."))
+
+    def test_enable_Acunetix_Scan_scanner(self):
+        driver = self.driver
+        driver.get(self.base_url + "tool_type")
+        driver.find_element_by_link_text("Acunetix Scan").click()
+        checkbox = driver.find_element_by_id("id_enabled")
+        if not checkbox.get_attribute('checked'):
+            checkbox.click()
+
+        driver.find_element_by_css_selector(".col-sm-offset-2 > .btn").click()
+        self.assertTrue(self.is_success_message_present(text="Tool Type Successfully Updated."))
+
+    def test_Acunetix_Scan_visibility(self):
+        driver = self.driver
+
+        self.test_disable_Acunetix_Scan_scanner
+
+        self.goto_product_overview(driver)
+        driver.find_element_by_css_selector(".dropdown-toggle.pull-left").click()
+        driver.find_element_by_link_text("Add New Engagement").click()
+        driver.find_element_by_id("id_name").send_keys("Scan Visibility Test")
+        driver.find_element_by_xpath('//*[@id="id_deduplication_on_engagement"]').click()
+        driver.find_element_by_name("_Import Scan Results").click()
+
+        self.assertTrue("Acunetix_Scan" not in self.driver.find_element_by_id("id_scan_type").text)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(BaseTestCase('test_login'))
+    suite.addTest(ScanTypeTest('test_disable_Acunetix_Scan_scanner'))
+    suite.addTest(ScanTypeTest('test_enable_Acunetix_Scan_scanner'))
+    suite.addTest(ScanTypeTest('test_Acunetix_Scan_visibility'))
+    #suite.addTest(ScanTypeTest('test_Acunetix_Scan_visibility_enable'))
+    return suite
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(descriptions=True, failfast=True, verbosity=2)
+    ret = not runner.run(suite()).wasSuccessful()
+    BaseTestCase.tearDownDriver()
+    sys.exit(ret)
